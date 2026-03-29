6296d475df37c98369ed1e2b1623bd5b7ad7bd0a
Ximena Prado Blanco <41089488+ximenaprado@users.noreply.github.com>
2026-03-28 23:36:00 -0300

    Add tests for clasificar_temperatura ejercicio2

---
+++ tests/test_Ejercicio2.py
@@ -0,0 +1,13 @@
+from src.ejercicio2 import clasificar_temperatura
+
+def test_fria():
+    assert clasificar_temperatura(5) == "Fría", \
+        "Recuerda: temperaturas menores a 10°C son Frías."
+
+def test_templada():
+    assert clasificar_temperatura(20) == "Templada", \
+        "Entre 10 y 25 grados debe clasificarse como Templada."
+
+def test_calurosa():
+    assert clasificar_temperatura(30) == "Calurosa", \
+        "Temperaturas mayores a 25°C son Calurosas."
