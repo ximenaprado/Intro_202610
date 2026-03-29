f06948faa10401783d64b460033396c894e61add
Ximena Prado Blanco <41089488+ximenaprado@users.noreply.github.com>
2026-03-28 23:52:15 -0300

    Add tests for contar_asistencias function

---
+++ Semana5/tests/test_Ejercicio4.py
@@ -0,0 +1,9 @@
+from src.ejercicio4 import contar_asistencias
+
+def test_asistencias():
+    assert contar_asistencias([1, 1, 0]) == 2, \
+        "Debes contar solo los valores 1 hasta que aparezca el 0."
+
+def test_sin_asistencia():
+    assert contar_asistencias([0]) == 0, \
+        "Si el primer valor es 0, el total de asistencias debe ser 0."
