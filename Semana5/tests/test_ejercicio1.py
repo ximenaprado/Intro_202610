56670b8284e010a125a28a27a759b28c0da7d012
Ximena Prado Blanco <41089488+ximenaprado@users.noreply.github.com>
2026-03-28 23:53:44 -0300

    Fix import path for validar_contraseña function

--- Semana5/tests/test_ejercicio1.py
+++ Semana5/tests/test_ejercicio1.py
@@ -1,4 +1,4 @@
-from src.ejercicio1 import validar_contraseña
+from semana5.ejercicio1 import validar_contraseña
 
 def test_contraseña_correcta():
     assert validar_contraseña(["123", "abc123"]) == "Acceso concedido", \
